5b52732e0acd3567c3bc0ddf8caaf7cba8db2a34
Isak Gaardsted Møller <ism001@edu.zealand.dk>
2024-10-01 10:35:31 +0200

    Task 3

---
+++ TicketClassLibrary/MC.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary
+{
+    public class MC
+    {
+        /// <summary>
+        /// Får eller sætter nummerpladen på køretøjet.
+        /// </summary>
+        public string LicensePlate { get; set; }
+
+        /// <summary>
+        /// Får eller sætter datoen for transaktionen.
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Returnerer prisen for køretøjet.
+        /// </summary>
+        /// <returns>Prisen for køretøjet.</returns>
+        public double Price()
+        {
+            double price = 125;
+            return price;
+        }
+
+        /// <summary>
+        /// Returnerer køretøjstypen.
+        /// </summary>
+        /// <returns>En streng, der repræsenterer køretøjstypen.</returns>
+        public string VehicleType()
+        {
+            string mc = "MC";
+            return mc;
+        }
+    }
+}
---
+++ TicketClassLibraryTests/MCTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TicketClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketClassLibrary.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod]
+        public void LicensePlate_SetAndGet_ReturnsCorrectValue()
+        {
+            // Arrange
+            var vehicle = new MC();
+            string expectedLicensePlate = "AB12345";
+
+            // Act
+            vehicle.LicensePlate = expectedLicensePlate;
+            string actualLicensePlate = vehicle.LicensePlate;
+
+            // Assert
+            Assert.AreEqual(expectedLicensePlate, actualLicensePlate);
+        }
+
+        [TestMethod]
+        public void Date_SetAndGet_ReturnsCorrectValue()
+        {
+            // Arrange
+            var vehicle = new MC();
+            DateTime expectedDate = new DateTime(2023, 10, 1);
+
+            // Act
+            vehicle.Date = expectedDate;
+            DateTime actualDate = vehicle.Date;
+
+            // Assert
+            Assert.AreEqual(expectedDate, actualDate);
+        }
+
+        [TestMethod]
+        public void Price_ReturnsCorrectValue()
+        {
+            // Arrange
+            var vehicle = new MC();
+            double expectedPrice = 125;
+
+            // Act
+            double actualPrice = vehicle.Price();
+
+            // Assert
+            Assert.AreEqual(expectedPrice, actualPrice);
+        }
+
+        [TestMethod]
+        public void VehicleType_ReturnsCorrectType()
+        {
+            // Arrange
+            var vehicle = new MC();
+            string expectedType = "MC";
+
+            // Act
+            string actualType = vehicle.VehicleType();
+
+            // Assert
+            Assert.AreEqual(expectedType, actualType);
+        }
+        
+        
+    }
+}
